84edc9a7c95a4d1789cc65b546700c62f61c85fd
Justin Tout <3639630+justintout@users.noreply.github.com>
2020-01-15 02:20:08 -0500

    initialize

---
+++ bin/character-list.ts
@@ -0,0 +1,48 @@
+#!npx ts-node
+import path from 'path';
+import glob from 'glob';
+import puppeteer from 'puppeteer';
+//@ts-ignore
+import replace from 'replace-in-file';
+import Bundler, { ParcelOptions } from 'parcel-bundler';
+
+(async () => {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+
+    const charactersOn = async function(p: string): Promise<string[]> {
+        await page.goto(`localhost:1234/${p}`);
+        //@ts-ignore
+        const text: string = await page.$eval('*', (el) => el.innerText);
+        return text.split('');   
+    }
+
+    glob("src/**.html", async (_, files) => {
+        console.log(files.map(x=>path.basename(x)));
+        const bundlerOptions: ParcelOptions = {
+            // @ts-ignore
+            logLevel: 4,
+        }
+        const bundler = new Bundler(files, bundlerOptions);
+        const server = await bundler.serve(1234, false, 'localhost');
+        
+        for (let p of files.map(x=>path.basename(x))) {
+            try {
+                const letterSet = new Set<string>();
+                const letters = await charactersOn(path.basename(p));
+                letters.forEach(l => letterSet.add(l));
+                const s = Array.from(letterSet.values())
+                    .join('')
+                    .replace(/\s/g, '');
+                const results = await replace({
+                    files: `dist/${p}`,
+                    from: /&display=swap/,
+                    to: `&display=swap&text=${s}`
+                });
+            } catch (e) {
+                console.log(e);
+            }
+        }
+        process.exit(0);
+    });
+})();
---
+++ src/main.ts
@@ -0,0 +1,52 @@
+window.addEventListener('hashchange', (event) => {
+    const id = window.location.hash.slice(1);
+    const prev = event.oldURL.includes('#') ? event.oldURL.split('#')[1] : '';
+    if (prev == '' && id.length) {
+        console.log(`hope you enjoy your first article of the visit, "${titleFromSlug(id)} :)"`);
+    }
+    expandArticle(id);
+});
+
+function titleFromSlug(slug: string): string {
+    const elems = Array.from(document.querySelectorAll<HTMLHeadingElement>('article > h2'))
+        .filter((n) => n.id.toLowerCase() === slug.toLowerCase());
+    if (elems.length == 0) {
+        return '';
+    }
+    if (elems.length > 1) {
+        console.log(`i have two articles with the same slug! i better fix that...`);
+    }
+    if (elems[0].textContent == null) {
+        return '';
+    } 
+    return elems[0].textContent;
+}
+
+function expandArticle(slug: string) {
+    if (slug == '') {
+        return;
+    }
+    document.querySelector<HTMLHeadingElement>(`#${slug}`)!.classList.replace('collapsed', 'expanded');
+    const a = document.querySelector<HTMLAnchorElement>(`#${slug} ~ a.article-expander`);
+    if (a) {
+        a.text = '...enough';
+        a.addEventListener('click', () => {
+            collapseArticle(slug);
+            a.addEventListener('click', () => {
+                expandArticle(slug);
+            }, {once: true});
+            return true;
+        }, {once: true});
+    }
+}
+
+function collapseArticle(slug: string) {
+    if (slug == '') {
+        return;
+    }
+    document.querySelector<HTMLHeadingElement>(`#${slug}`)!.classList.replace('expanded', 'collapsed');
+    const a = document.querySelector<HTMLAnchorElement>(`#${slug} ~ a.article-expander`);
+    if (a) {
+        a.text = 'more...';
+    }
+}
